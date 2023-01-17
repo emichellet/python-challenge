5ed82a8d99aac7c0b5189fda0c858e1019a1c598
emichellet <emichelletan@gmail.com>
2023-01-16 23:39:28 -0500

    test commit
    initial commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,29 @@
     PyPoll:
 
-    
+        Assignment Instructions:
+        Create a Python script that analyzes the votes of a small, rural town.
+
+        Analyze the votes and calculate each of the following values:
+        - The total number of votes cast 
+        - A complete list of candidates who received votes
+        - The percentage of votes that each candidate won
+        - The total number of votes that each candidate won
+        - The winner of the election based on popular vote
+
+/////
+
+#   Import all the required modules
+import os
+import csv
+import operator
+
+
+#   Set a path for the file 
+csvpath = os.path.join("Resouces", "pypoll_election_data.csv")
+tot_votes = 0
+candidate_names = []
+candidate_names_and_votes_dict = {}
+summary_of_candidates = ""
+winner_name = []
+
+
