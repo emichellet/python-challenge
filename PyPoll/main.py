5b597786d94a2671561e88fdf3fc699ea85fd960
emichellet <emichelletan@gmail.com>
2023-01-16 23:53:21 -0500

    test commit
    initial commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,3 +27,46 @@ summary_of_candidates = ""
 winner_name = []
 
 
+    with open(csvpath) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        header = next(csvreader)
+
+        for candidate_data in csvreader"
+        
+        #   Find the total number of votes cast
+        #       Add the total of all rows (disregard the header row)
+        tot_votes += 1
+
+        #   List out all the unique candidates
+        #   Start a dictionary with said candidates (note: set values to zero)
+        if candidate_data[2] not in candidate_names
+            candidate_names.append(candidate_data[2])
+            candidate_names_and_votes_dict[candidate_data[2]] = 0
+
+        #   Add the total votes to the dictionary keys
+        candidate_names_and_votes_dict[candidate_data[2]] += 1
+
+    #   Create a loop to get the percent values and
+    #   combine all the variables from the dictionary into the string to present in the output
+    for the key, value in candidate_names_and_votes_dict.items():
+        vote_percentage = ((value/tot_votes)*100)
+        summary_of_candidates += f"{key}: {vote_percentage.3f}% ({value})\n"
+
+    
+    #   Output: printed in txt file within the analysis folder
+    output = (
+            f"Election Results\n"
+            f"-----------------\n"
+            f"Total votes: {tot_votes}\n"
+            f"-----------------\n"
+            f"Summary for each candidate: {summary_of_candidates}"
+            f"------------------\n"
+            f"the Winner (of the election): {winner_name}\n"
+            f"------------------\n")
+
+    #   Print out the results to the terminal
+    print(output)
+
+    #   Print out all the outputs in the txt file within the analysis folder
+    with open("analysis/output.txt", "w") as txt_file"
+        txt_file.write(output)
